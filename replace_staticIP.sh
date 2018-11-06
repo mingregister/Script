7bfa4d729774543f6602e7c76892b12d73e7037e
mingregister <774262573@qq.com>
2018-11-06 10:19:46 +0800

    optimize replace_staticIP.sh

--- replace_staticIP.sh
+++ replace_staticIP.sh
@@ -4,11 +4,9 @@ set -eu
 # set -x
 set -o pipefail
 
+# CONFIGFILE=/tmp/ifcfg-ens33
 CONFIGFILE=/etc/sysconfig/network-scripts/ifcfg-ens33
-MASK=''
-IP=''
-GATEWAY=''
-
+ 
 function usage() {
   cat >&2 <<-'EOF'
     USAGE：
@@ -18,39 +16,33 @@ function usage() {
 EOF
 }
 
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --ip | -i)
-      IP=$2
-      shift             # 如果是参数组的话，加上这个shift，如果不是，那就不需要加。
-      ;;
-  --mask | -m)
-      MASK=$2
-      shift
-      ;;
-  --gateway | -g)
-      GATEWAY=$2
-      shift
-      ;;
-  --* | -* | * | -h)
-      echo "Illegal option $1"
-      usage
-      exit 1
-      ;;
-  esac
-  shift $(( $# > 0 ? 1 : 0 ))
+DEFAULTIP=''
+DEFAULTMASK=255.255.255.0
+DEFAULTGATEWAY=192.168.160.2
+
+while getopts "i:m:g:" opt; do
+    case ${opt} in
+        i )
+            ip=${OPTARG}
+            ;;
+        m )
+            mask=${OPTARG}
+            ;;
+        g )
+            gateway=${OPTARG}
+            ;;            
+    esac
 done
+shift $((OPTIND-1))
+
+IP=${ip:-${DEFAULTIP}}
+MASK=${mask:-${DEFAULTMASK}}
+GATEWAY=${gateway:-${DEFAULTGATEWAY}}
 
 if [[ -z ${IP} ]]; then
   echo "IP must be passed"
-fi
-
-if [[ -z ${MASK} ]]; then
-  MASK=255.255.255.0
-fi
-
-if [[ -z ${GATEWAY} ]]; then
-  GATEWAY=192.168.160.1
+  usage
+  exit 1
 fi
 
 sed -i "s/^BOOTPROTO.*/BOOTPROTO=static/" ${CONFIGFILE}
